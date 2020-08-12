a1afa39993c411cd6e00181609227adf50e112d7
xinyi <xinyiaw@live.com.sg>
2020-08-12 17:37:26 +0800

    bug fix - medication

--- components/hypofood/viewMore.js
+++ components/hypofood/viewMore.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, StyleSheet, Text, Image} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
+import SampleData from '../../screens/more/sampleHypoList.json';
 
+//using sample data for now
 const ViewMore = (props) => {
-  const {category} = props;
+  const {category} = props.route.params;
+  const [arr, setArr] = useState([]);
 
-  return (
-    <View style={styles.screen}>
-      <Text>hihi</Text>
-    </View>
-  );
+  useEffect(() => {
+    //initialise list of items under same category
+    for (var x in SampleData.data) {
+      if (x.category === category) {
+        setArr(x.foodItems);
+      }
+    }
+  }, []);
+
+  return <View style={styles.screen}></View>;
 };
 
 export default ViewMore;
