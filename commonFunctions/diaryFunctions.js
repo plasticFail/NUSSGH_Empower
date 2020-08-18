6189f7bd93c56b4469d0b3fba68c2264a37bd89c
xinyi <xinyiaw@live.com.sg>
2020-08-18 17:39:40 +0800

    Update diaryFunctions.js

--- commonFunctions/diaryFunctions.js
+++ commonFunctions/diaryFunctions.js
@@ -8,17 +8,21 @@ const getDateObj = (dateString) => {
 };
 
 const getTime = (dateString) => {
-  let time = dateString.substring(
-    dateString.indexOf('2020') + 4,
-    dateString.length - 3,
-  );
-  return time;
+  if (dateString != null) {
+    let time = dateString.substring(
+      dateString.indexOf('2020') + 4,
+      dateString.length - 3,
+    );
+    return time;
+  }
 };
 
 const getHour = (dateString) => {
-  let timeString = getTime(dateString);
-  let hourString = timeString.substring(0, 6).trim().replace(/[:]/, '');
-  return Number(hourString);
+  if (dateString != null) {
+    let timeString = getTime(dateString);
+    let hourString = timeString.substring(0, 6).trim().replace(/[:]/, '');
+    return Number(hourString);
+  }
 };
 
 export {getDateObj, getTime, getHour};
