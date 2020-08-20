5defc3700a68bc77406594c7be48a7e42402fa24
vincent <vincent71399@gmail.com>
2020-08-20 13:30:37 +0800

    -fix radio btn form not two way binding issue

---
+++ components/logs/formBlockFix.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import RadioButton from '../radioButton';
+
+const FormBlockFix = (props) => {
+  return (
+    <View>
+      <Text style={styles.questionHeader}>{props.question}</Text>
+      <View style={styles.buttonGroupStyle}>
+        <TouchableOpacity onPress={() => props.getFormSelection(true)}>
+          <RadioButton
+            btnText={'Yes'}
+            color={props.color}
+            selected={props.selectYes}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{marginStart: '40%'}}
+          onPress={() => props.getFormSelection(false)}>
+          <RadioButton btnText={'No'} color={props.color} selected={!props.selectYes}/>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default FormBlockFix;
+
+const styles = StyleSheet.create({
+  questionHeader: {
+    fontSize: 17,
+    fontWeight: '500',
+    color: 'black',
+    marginTop: '5%',
+    marginStart: '2%',
+  },
+  buttonGroupStyle: {
+    flexDirection: 'row',
+    marginTop: ' 5%',
+    marginStart: '5%',
+  },
+});
