30bae815f14bb58500536bdd89c7a9f469f23d95
admin <yijieelim@gmail.com>
2020-09-18 10:34:04 +0800

    revert?

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const { getDefaultConfig } = require("metro-config");
 /**
  * Metro configuration for React Native
  * https://github.com/facebook/react-native
@@ -5,6 +6,28 @@
  * @format
  */
 
+module.exports = (async () => {
+  const {
+    resolver: { sourceExts, assetExts }
+  } = await getDefaultConfig();
+  return {
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: false
+        }
+      }),
+      babelTransformerPath: require.resolve("react-native-svg-transformer")
+    },
+    resolver: {
+      assetExts: assetExts.filter(ext => ext !== "svg"),
+      sourceExts: [...sourceExts, "svg"]
+    }
+  };
+})();
+
+/*
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
@@ -15,3 +38,4 @@ module.exports = {
     }),
   },
 };
+ */
