2f8b6f89b2ba47cccf3a1dda84e111966e4fb741
xinyi <xinyiaw@live.com.sg>
2020-09-25 15:59:45 +0800

    long press counter

--- components/stepCounter.js
+++ components/stepCounter.js
@@ -1,7 +1,8 @@
-import React, {useState} from 'react';
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useState, Component} from 'react';
+import {View, Text, StyleSheet, TouchableWithoutFeedback} from 'react-native';
 //third party lib
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 //Props:
 //paramter: eg. 1 unit - parameter is unit
@@ -9,45 +10,65 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 //textStyle: style the text of the count
 Ionicons.loadFont();
 
-const StepCounter = (props) => {
-  const {parameter, textStyle} = props;
-  const {count} = props === undefined ? 0 : props;
+export default class StepCounter extends Component {
+  constructor(props) {
+    super(props);
+    this.props = props;
+    this.state = {
+      count: 0,
+    };
+    this.timer = null;
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleMinus = this.handleMinus.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+  }
 
-  const handleAdd = () => {
-    let newCount = count + 1;
-    props.setCount(newCount);
+  handleAdd = () => {
+    let newCount = this.state.count + 1;
+    this.setState({count: newCount});
+    this.timer = setTimeout(this.handleAdd, 200);
+    this.props.setCount(newCount);
   };
 
-  const handleMinus = () => {
-    if (count > 0) {
-      let newCount = count - 1;
-      props.setCount(newCount);
+  handleMinus = () => {
+    clearTimeout();
+    if (this.state.count > 0) {
+      let newCount = this.state.count - 1;
+      this.setState({count: newCount});
+      this.timer = setTimeout(this.handleMinus, 200);
+      this.props.setCount(newCount);
     } else {
-      props.setCount(0);
+      this.setState({count: 0});
     }
   };
 
-  return (
-    <View style={{...styles.container, ...props.style}}>
-      <TouchableOpacity onPress={handleMinus}>
-        <Ionicons
-          name="remove-circle"
-          size={40}
-          color={'#aad326'}
-          style={{flex: 1}}
-        />
-      </TouchableOpacity>
-      <Text style={[styles.countContent, textStyle]}>
-        {count} {parameter}
-      </Text>
-      <TouchableOpacity onPress={handleAdd}>
-        <Ionicons name="add-circle" size={40} color={'#aad326'} />
-      </TouchableOpacity>
-    </View>
-  );
-};
+  stopTimer() {
+    console.log('calling');
+    clearTimeout(this.timer);
+  }
 
-export default StepCounter;
+  render() {
+    const {parameter, textStyle} = this.props;
+    const {count} = this.state;
+    return (
+      <View style={{...styles.container, ...this.props.style}}>
+        <TouchableOpacity
+          onPressIn={this.handleMinus}
+          onPressOut={this.stopTimer}>
+          <Ionicons name="remove-circle" size={40} color={'#aad326'} />
+        </TouchableOpacity>
+        <Text style={[styles.countContent, textStyle]}>
+          {count} {parameter}
+        </Text>
+        <TouchableOpacity
+          onPressIn={this.handleAdd}
+          onPressOut={this.stopTimer}>
+          <Ionicons name="add-circle" size={40} color={'#aad326'} />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
