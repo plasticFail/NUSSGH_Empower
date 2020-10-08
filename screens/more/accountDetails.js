50606904596a678b7ade658305c750b5a5b2cb46
xinyi <xinyiaw@live.com.sg>
2020-10-08 10:13:35 +0800

    minor bug fix

--- screens/more/accountDetails.js
+++ screens/more/accountDetails.js
@@ -67,7 +67,6 @@ const AccountDetailScreen = (props) => {
           closeModal={() => setPasswordModalVisible(false)}
         />
 
-        {/*
         <Clickable
           heading="Medication Plan"
           content=""
@@ -76,7 +75,6 @@ const AccountDetailScreen = (props) => {
           closeModal={() => {}}
           modalVisible={false}
         />
-        */}
 
         <View style={{flexDirection: 'row', margin: '2%'}}>
           <Entypo name="link" size={30} color={Colors.lastLogValueColor} />
