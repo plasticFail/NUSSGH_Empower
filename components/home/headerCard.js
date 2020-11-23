192d8995bf16214fff7509261d5f5d52957386b7
vincent <vincent71399@gmail.com>
2020-11-23 16:50:55 +0800

    -merge changes

--- components/home/headerCard.js
+++ components/home/headerCard.js
@@ -4,7 +4,6 @@ import {View, Text, StyleSheet} from 'react-native';
 import {Colors} from '../../styles/colors';
 import {adjustSize} from '../../commonFunctions/autoResizeFuncs';
 
-
 const HeaderCard = (props) => {
   const {username, hour} = props;
 
@@ -28,47 +27,11 @@ const styles = StyleSheet.create({
     fontFamily: 'SFProDisplay-Bold',
     marginStart: '5%',
   },
-  taskText: {
-    fontFamily: 'SFProDisplay-Regular',
-    color: 'white',
-    marginStart: '5%',
-    fontSize: adjustSize(18),
-  },
-  bold: {
-    fontFamily: 'SFProDisplay-Bold',
-    color: 'white',
-    marginStart: '5%',
-    fontSize: adjustSize(18),
-  },
-  logCard: {
-    backgroundColor: 'white',
-    borderRadius: adjustSize(9.5),
-    marginTop: '3%',
-    marginStart: '5%',
-    marginEnd: '5%',
-    padding: '3%',
-  },
-  logLogo: {
-    marginEnd: '3%',
-    marginStart: '5%',
-    marginTop: '3%',
-  },
   usernameText: {
     color: '#16A750',
     fontSize: adjustSize(40),
     fontFamily: 'SFProDisplay-Bold',
     marginStart: '5%',
-  },
-  buttonStyle: {
-    backgroundColor: '#aad326',
-    padding: '2%',
-    borderRadius: adjustSize(20),
-    margin: '4%',
-  },
-  buttonText: {
-    fontSize: adjustSize(18),
-    fontWeight: '700',
-    textAlign: 'center',
   },
   chevron: {
     marginTop: '5%',
